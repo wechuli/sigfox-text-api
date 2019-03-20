b774ad1e4707e4a6ed4e7f6d1f9a102c92b6125c
wechuli <wechulipaul@yahoo.com>
2019-03-20 14:55:48 +0300

    log body

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(bodyParser.json());
 
 app.use("/data", (req, res) => {
   try {
-    console.log(req.params);
-    console.log(req.query);
+    // console.log(req.params);
+    // console.log(req.query);
     console.log(req.body);
 
     res.status(200).json({ success: true });
