4e641cf5541aa756d418987dcdca79c8da06f563
wechuli <wechulipaul@yahoo.com>
2019-03-20 14:43:09 +0300

    sigfox dummy test api

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+//Initialize tha app
+
+const app = express();
+
+//Add middleware
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Database connection
+
+//Routes
+
+app.use("/data", (req, res) => {
+  try {
+    console.log(req.params);
+    console.log(req.query);
+    console.log(req.body);
+
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+//404 handler
+app.use((req, res) => {
+  res.status(404).json({ error: "Resource Unaivalable" });
+});
+
+//Listening
+const port = process.env.PORT || 5002;
+
+app.listen(port, () => {
+  console.info(`The app is listening on port ${port}`);
+});
